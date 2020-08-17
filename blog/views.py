86131b037f96bf2adaa5b0aad48ec7c06d255cc5
rishabhrj11 <rishabhjain056@gmail.com>
2020-08-17 15:08:31 +0530

    Added Resume and profile picture and Blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Blog
+def blog(request):
+    blogs = Blog.objects.order_by('date')[:5]
+    return render(request,'blogs/blog.html',{"blogs":blogs})
