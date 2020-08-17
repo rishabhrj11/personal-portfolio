d18708b7396434ba1f29dac53ce1e302631e6f02
rishabhrj11 <rishabhjain056@gmail.com>
2020-08-17 16:34:06 +0530

    All functionalities except CSS

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('',views.blog,name = 'blog'),
+    path('<int:blog_id>/',views.detail,name = 'detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 def blog(request):
     blogs = Blog.objects.order_by('date')[:5]
     return render(request,'blogs/blog.html',{"blogs":blogs})
+
+def detail(request,blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request,'blogs/detail.html',{'blog':blog})
